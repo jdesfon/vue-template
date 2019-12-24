fe3e630fedf3792e69b33a752e65dfcb6bf66fb1
jean-michel-dot-fox <jean-michel@foxintelligence.fr>
2019-12-24 23:31:34 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <div id="app">
+    <v-app>
+      <v-content>
+        <router-view/>
+      </v-content>
+    </v-app>
+  </div>
+</template>
---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    hello
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home',
+};
+</script>
+
+<style lang="scss" scoped>
+.home {
+  color: $ibis;
+}
+</style>
